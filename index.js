206f55ec6ef49373708efe4ee4dad606ba7e7378
Sheen Carbellido <sheencarbellido@live.com>
2022-05-24 13:13:25 +0800

    Add cors

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 const PORT = 9001;
 const graphql = require("graphql");
 const schema = require("./Schemas");
+const cors = require("cors");
 
 const { graphqlHTTP } = require("express-graphql");
 
+app.use(cors()); // enable `cors` to set HTTP response header: Access-Control-Allow-Origin: *
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 app.listen(PORT, () => {
   console.log("Server running");
